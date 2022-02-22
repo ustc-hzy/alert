0cc753b0b5582c9f1a0f50593e8a8ea26a9fd296
lan <1312489009@qq.com>
2022-02-22 01:45:41 -0800

    task

--- core/service/task_service.go
+++ core/service/task_service.go
@@ -1,7 +1,28 @@
 package service
 
-import "time"
+import (
+	"alert/core/dao/task_dao"
+	"time"
+)
 
 type TaskServiceImpl struct{}
 
-func (i TaskServiceImpl) Schedule(frequency time.Duration) {}
+func (i TaskServiceImpl) Add(TaskCode string, TaskName string, RuleCode string, Frequency time.Duration, NextTime time.Time, Status bool) bool {
+	return false
+}
+
+func (i TaskServiceImpl) Delete(TaskCode string) bool {
+	return false
+}
+
+func (i TaskServiceImpl) Query(TaskCode string) task_dao.Task {
+	return task_dao.Task{}
+}
+
+func (i TaskServiceImpl) Modify(TaskCode string, TaskName string, RuleCode string, Frequency time.Duration, NextTime time.Time, Status bool) bool {
+	return false
+}
+
+func (i TaskServiceImpl) UpdateTime() bool {
+	return false
+}
