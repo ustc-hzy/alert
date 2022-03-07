e7689769823afda3e76ddfc5cb9686b5d703a155
hzy <1059144380@qq.com>
2022-03-07 02:24:27 +0000

    compound inds rule

--- core/service/indicator_service_test.go
+++ core/service/indicator_service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"alert/core"
 	"alert/core/dao/indicator_dao"
+	"alert/core/vo"
 	"fmt"
 	"testing"
 	"time"
@@ -13,8 +14,19 @@ var ind_compute_service = IndComputeImpl{}
 
 func TestAdd(t *testing.T) {
 
-	indicator := indicator_dao.Indicator{IndicatorCode: "test7", Name: "test", Expression: "test", Description: "test", CreateTime: time.Now(), UpdateTime: time.Now()}
-	indicatorJson := indicator_dao.IndicatorJson{Indicators: nil, Caculate: core.NIL, Value: "select deal_amount from `deal_infos`"}
+	indicator := indicator_dao.Indicator{
+		IndicatorCode: "test7",
+		Name:          "test",
+		Expression:    "test",
+		Description:   "test",
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+	indicatorJson := indicator_dao.IndicatorJson{
+		Indicators: nil,
+		Caculate:   core.NIL,
+		Value:      "select deal_amount from `deal_infos`",
+	}
 	result := indicator_service.Add(indicator, indicatorJson)
 	if !result {
 		t.Fatal("error")
@@ -36,11 +48,70 @@ func TestQuery(t *testing.T) {
 }
 
 func TestModify(t *testing.T) {
-	indicator := indicator_dao.Indicator{IndicatorCode: "test", Name: "test", Description: "modify", UpdateTime: time.Now()}
+	indicator := indicator_dao.Indicator{
+		IndicatorCode: "test",
+		Name:          "test",
+		Description:   "modify",
+		UpdateTime:    time.Now(),
+	}
 	indicator_service.Modify(indicator)
 }
 
 func TestCompute(t *testing.T) {
 	val := ind_compute_service.Compute("test2", 0, time.Now(), time.Now())
+	fmt.Println(val)
+}
+
+func TestAddCompound(t *testing.T) {
+
+	indicatorA := vo.IndicatorVO{
+		IndicatorCode: "nodeA",
+		Name:          "test",
+		Indicators:    nil,
+		Caculate:      core.NIL,
+		Value:         "select deal_amount from `deal_infos`",
+		Description:   "test",
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+	indicatorB := vo.IndicatorVO{
+		IndicatorCode: "nodeB",
+		Name:          "test",
+		Indicators:    nil,
+		Caculate:      core.NIL,
+		Value:         "select deal_amount from `deal_infos`",
+		Description:   "test",
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+
+	indicator := indicator_dao.Indicator{
+		IndicatorCode: "nodeALL",
+		Name:          "test",
+		Expression:    "test",
+		Description:   "test",
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+	inds := []vo.IndicatorVO{indicatorA, indicatorB}
+	indicatorJson := indicator_dao.IndicatorJson{
+		Indicators: inds,
+		Caculate:   core.ADD,
+		Value:      "",
+	}
+
+	result := indicator_service.Add(indicator, indicatorJson)
+	if !result {
+		t.Fatal("error")
+	}
+}
+
+func TestComputeCompound(t *testing.T) {
+	val := ind_compute_service.Compute(
+		"nodeALL",
+		0,
+		time.Now(),
+		time.Now(),
+	)
 	fmt.Println(val)
 }
--- core/service/rule_service_test.go
+++ core/service/rule_service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"alert/core"
 	"alert/core/dao/rule_dao"
+	"alert/core/vo"
 	"fmt"
 	"testing"
 	"time"
@@ -50,5 +51,69 @@ func TestRuleQuery(t *testing.T) {
 func TestCheck(t *testing.T) {
 
 	val, _ := rule_check_service.Check("test12")
+	fmt.Println(val)
+}
+
+func TestRuleAddCompound(t *testing.T) {
+
+	ruleA := vo.RuleVo{
+		RuleCode:      "ruleA",
+		RuleName:      "ruleA",
+		RoomId:        0,
+		Rules:         nil,
+		Logic:         core.NIL,
+		Op:            core.EQUAL,
+		Value:         0,
+		IndicatorCode: "test6",
+		Description:   "test",
+		StartTime:     time.Now(),
+		EndTime:       time.Now(),
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+	ruleB := vo.RuleVo{
+		RuleCode:      "ruleB",
+		RuleName:      "ruleB",
+		RoomId:        0,
+		Rules:         nil,
+		Logic:         core.NIL,
+		Op:            core.EQUAL,
+		Value:         4,
+		IndicatorCode: "nodeALL",
+		Description:   "test",
+		StartTime:     time.Now(),
+		EndTime:       time.Now(),
+		CreateTime:    time.Now(),
+		UpdateTime:    time.Now(),
+	}
+
+	rule := rule_dao.Rule{
+		RuleCode:    "ruleAll2",
+		RuleName:    "ruleAll2",
+		RoomId:      0,
+		Expression:  "test",
+		Description: "test",
+		StartTime:   time.Now(),
+		EndTime:     time.Now(),
+		CreateTime:  time.Now(),
+		UpdateTime:  time.Now(),
+	}
+	rules := []vo.RuleVo{ruleA, ruleB}
+	ruleJson := rule_dao.RuleJson{
+		Rules:         rules,
+		Logic:         core.OR,
+		Op:            core.NIL,
+		Value:         0,
+		IndicatorCode: "",
+	}
+
+	result := rule_service.Add(rule, ruleJson)
+	if !result {
+		t.Fatal("error")
+	}
+}
+
+func TestRuleComputeCompound(t *testing.T) {
+	val, _ := rule_check_service.Check("ruleAll2")
 	fmt.Println(val)
 }
