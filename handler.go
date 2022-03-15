f446c41e2658946bd568798e0c80bbcf43a05ca9
hzy <1059144380@qq.com>
2022-03-15 08:35:12 +0000

    handler all

--- handler.go
+++ handler.go
@@ -3,8 +3,8 @@ package main
 import (
 	"alert/core/dao/indicator_dao"
 	"alert/core/dao/rule_dao"
-	"alert/core/service"
 	"alert/core/dao/task_dao"
+	"alert/core/service"
 	"alert/kitex_gen/api"
 	"context"
 	"github.com/jinzhu/copier"
@@ -14,23 +14,23 @@ import (
 // CRUDImpl implements the last service interface defined in the IDL.
 type CRUDImpl struct{}
 
+const TIMETAMPLATE = "2006-01-02 15:04:05"
+
 // AddIndicator implements the CRUDImpl interface.
 func (s *CRUDImpl) AddIndicator(ctx context.Context, req *api.AddIndicatorRequest) (resp *api.AddIndicatorResponse, err error) {
-	// TODO: Your code here...
 	indicator, indicatorJson := ConvertIndicator(req)
-	service.IndicatorServiceImpl{}.Add(indicator, indicatorJson)
-	return
+	res := service.IndicatorServiceImpl{}.Add(indicator, indicatorJson)
+	return &api.AddIndicatorResponse{Success: res}, nil
 }
 
 func ConvertIndicator(req *api.AddIndicatorRequest) (indicator_dao.Indicator, indicator_dao.IndicatorJson) {
 	indicator := indicator_dao.Indicator{}
 	indicatorJson := indicator_dao.IndicatorJson{}
 	copier.Copy(&indicator, req.Indicator)
-	timeTemplate := "2006-01-02 15:04:05"
 	createTimeStr := req.Indicator.GetTimeCreate()
 	updateTimeStr := req.Indicator.GetTimeUpdate()
-	createTime, _ := time.ParseInLocation(timeTemplate, createTimeStr, time.Local)
-	updateTime, _ := time.ParseInLocation(timeTemplate, updateTimeStr, time.Local)
+	createTime, _ := time.ParseInLocation(TIMETAMPLATE, createTimeStr, time.Local)
+	updateTime, _ := time.ParseInLocation(TIMETAMPLATE, updateTimeStr, time.Local)
 	indicator.CreateTime = createTime
 	indicator.UpdateTime = updateTime
 	copier.Copy(&indicatorJson, req.IndicatorJson)
@@ -39,33 +39,33 @@ func ConvertIndicator(req *api.AddIndicatorRequest) (indicator_dao.Indicator, in
 
 // DeleteIndicator implements the CRUDImpl interface.
 func (s *CRUDImpl) DeleteIndicator(ctx context.Context, req *api.DeleteIndicatorRequest) (resp *api.DeleteIndicatorResponse, err error) {
-	// TODO: Your code here...
-	service.IndicatorServiceImpl{}.Delete(req.IndicatorCode)
-	return
+	res := service.IndicatorServiceImpl{}.Delete(req.IndicatorCode)
+	return &api.DeleteIndicatorResponse{Success: res}, nil
 }
 
 // QueryIndicator implements the CRUDImpl interface.
 func (s *CRUDImpl) QueryIndicator(ctx context.Context, req *api.QueryIndicatorRequest) (resp *api.QueryIndicatorResponse, err error) {
-	// TODO: Your code here...
-	service.IndicatorServiceImpl{}.Query(req.IndicatorCode)
-	return
+	res := service.IndicatorServiceImpl{}.Query(req.IndicatorCode)
+	indicator := api.Indicator{}
+	copier.Copy(&indicator, res)
+	indicator.TimeCreate = res.CreateTime.Format(TIMETAMPLATE)
+	indicator.TimeUpdate = res.UpdateTime.Format(TIMETAMPLATE)
+	return &api.QueryIndicatorResponse{Indicator: &indicator}, nil
 }
 
 // ModifyIndicator implements the CRUDImpl interface.
 func (s *CRUDImpl) ModifyIndicator(ctx context.Context, req *api.ModifyIndicatorRequest) (resp *api.ModifyIndicatorResponse, err error) {
-	// TODO: Your code here...
 	indicator := indicator_dao.Indicator{}
 	copier.Copy(&indicator, req.Indicator)
-	service.IndicatorServiceImpl{}.Modify(indicator)
-	return
+	res := service.IndicatorServiceImpl{}.Modify(indicator)
+	return &api.ModifyIndicatorResponse{Success: res}, nil
 }
 
 // AddRule implements the CRUDImpl interface.
 func (s *CRUDImpl) AddRule(ctx context.Context, req *api.AddRuleRequest) (resp *api.AddRuleResponse, err error) {
-	// TODO: Your code here...
 	rule, ruleJson := ConvertRule(req)
-	service.RuleServiceImpl{}.Add(rule, ruleJson)
-	return
+	res := service.RuleServiceImpl{}.Add(rule, ruleJson)
+	return &api.AddRuleResponse{Success: res}, nil
 }
 
 func ConvertRule(req *api.AddRuleRequest) (rule_dao.Rule, rule_dao.RuleJson) {
@@ -85,25 +85,26 @@ func ConvertRule(req *api.AddRuleRequest) (rule_dao.Rule, rule_dao.RuleJson) {
 
 // DeleteRule implements the CRUDImpl interface.
 func (s *CRUDImpl) DeleteRule(ctx context.Context, req *api.DeleteRuleRequest) (resp *api.DeleteRuleResponse, err error) {
-	// TODO: Your code here...
-	service.RuleServiceImpl{}.Delete(req.RuleCode)
-	return
+	res := service.RuleServiceImpl{}.Delete(req.RuleCode)
+	return &api.DeleteRuleResponse{Success: res}, nil
 }
 
 // QueryRule implements the CRUDImpl interface.
 func (s *CRUDImpl) QueryRule(ctx context.Context, req *api.QueryRuleRequest) (resp *api.QueryRuleResponse, err error) {
-	// TODO: Your code here...
-	service.RuleServiceImpl{}.Query(req.RuleCode)
-	return
+	res := service.RuleServiceImpl{}.Query(req.RuleCode)
+	rule := api.Rule{}
+	copier.Copy(&rule, res)
+	rule.TimeCreate = res.CreateTime.Format(TIMETAMPLATE)
+	rule.TimeUpdate = res.UpdateTime.Format(TIMETAMPLATE)
+	return &api.QueryRuleResponse{Rule: &rule}, nil
 }
 
 // ModifyRule implements the CRUDImpl interface.
 func (s *CRUDImpl) ModifyRule(ctx context.Context, req *api.ModifyRuleRequest) (resp *api.ModifyRuleResponse, err error) {
-	// TODO: Your code here...
 	rule := rule_dao.Rule{}
 	copier.Copy(&rule, req.Rule)
-	service.RuleServiceImpl{}.Modify(rule)
-	return
+	res := service.RuleServiceImpl{}.Modify(rule)
+	return &api.ModifyRuleResponse{Success: res}, nil
 }
 
 // AddTask implements the CRUDImpl interface.
